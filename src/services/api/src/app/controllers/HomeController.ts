68b6fe801119215a3227ca1c9c09e9c307cb7eca
eduardoaraya <eduardojezine@gmail.com>
2021-02-20 13:13:58 -0300

    Remove time logs

--- src/services/api/src/app/controllers/HomeController.ts
+++ src/services/api/src/app/controllers/HomeController.ts
@@ -1,19 +1,13 @@
 import { Request, Response, Router } from 'express';
 class HomeController {
   async index(req: Request, res: Response): Promise<void> {
-    console.time();
     res.render('pages/home');
-    console.timeEnd();
   }
   async signin(req: Request, res: Response): Promise<void> {
-    console.time();
     res.render('pages/signin');
-    console.timeEnd();
   }
   async signup(req: Request, res: Response): Promise<void> {
-    console.time();
     res.render('pages/signup');
-    console.timeEnd();
   }
 }
 export default new HomeController();
